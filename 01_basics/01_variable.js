4dd7348772c711da9033e5704c96443ba76a7c67
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-06-04 11:58:35 +0000

    Variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144558
+let accountEmail = "mohd@gmail.com"
+var accountPassword = "123456"
+accountCity = "Maharashtra"
+// accountId = 245 not allowed
+accountEmail = "wasique@google.com"
+accountPassword = "654321"
+accountCity = "Pune"
+let accountState;
+
+console.log(accountId);
+
+/* 
+   Preferred not use vae
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
