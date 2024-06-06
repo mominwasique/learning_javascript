a6be4fb9af1c338aeced9ecf8ec90714ec10dc0d
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-06-06 09:11:13 +0000

    DataTypes in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"  // treat all js code as newer version
+
+// alert (3 + 3)  // we are using nodejs, not browser
+
+console.log( 3 + 3)    // code readability should be high 
+
+console.log( "Wasique" );
+
+  // DATATYPES IN JAVASCRIPT
+
+let name = "Wasique"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+
+// object
+
+console.log(typeof "Wasique")
+console.log(typeof null)  
+// type of null is object
+
+console.log(typeof undefined) 
+// type of undefined is undefined
