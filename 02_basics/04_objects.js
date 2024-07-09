d9a499857642f34a3e916ad63eea3af4d5366a82
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-07-09 18:32:53 +0000

    Objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,9 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "Wasique"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = {} 
