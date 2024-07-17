76ea757b10803741ba426705c29e130ab29a60a7
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-07-17 16:19:40 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,4 +1,4 @@
-// +++++++++++++++ Function +++++++++++++++
+// +++++++++++++++ Functions +++++++++++++++
 
 // function addTwoNumbers(number1, number2){ ()parameter
 //     console.log(number1 + number2);
@@ -28,7 +28,37 @@ function loginUserMsg(username = "Sam"){
 
 function calculateCartPrice(...num1){ 
     //--> Rest Operator
+    // some time it is called Spread Operator
     return num1
 }
 
-console.log(calculateCartPrice(200,500));
+// console.log(calculateCartPrice(200,500,999,550));
+
+    // +++++++  Functions for Objects  +++++++     
+
+const user = {
+    username: "Wasique",
+    price: 199
+}
+
+function handleObject(anyObject){
+    // console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject(user)
+// handleObject({
+//     username: "Wasiq",
+//     price:399
+// })
+
+//    ++++++++ Functions for Arrays ++++++++
+
+const myNewArray = [100,200,300,400]
+
+function returnValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnValue(myNewArray));
+// console.log(returnValue([200,400,361,123]));
+
