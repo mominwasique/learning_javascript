89310e16eca195517203a09aede4432d03d8c1cc
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-07-18 20:17:06 +0000

    Changes

--- 01_basics/04_comparisonDataTypes.js
+++ 01_basics/04_comparisonDataTypes.js
@@ -3,23 +3,22 @@
 // console.log(2 >= 1);
 // console.log(2 < 1);
 // console.log(2 != 1);
-  // This compariso is very easy to understand
+  // This comparison is very easy to understand
 
-console.log("2" > 1);
-console.log("02" > 1);
-
-console.log(null > 0);
-console.log(null >= 0);
-console.log(null == 0);
+console.log("2" > 1); // true
+console.log("02" > 1); // true
+console.log(null > 0); // false
+console.log(null >= 0); // true
+console.log(null == 0); //false
 
 // The reason is that an equality check == and comparisons > < >= <= work differently.
 // Comparisons convert null to a number, treating it as 0.
 // That's why null >= 0 is true and null >0 is false.
 
-console.log(undefined > 0);
-console.log(undefined >= 0);
-console.log(undefined == 0);
+console.log(undefined > 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined == 0); // false
 
 // ===
 
-console.log("2" === 2);
+console.log("2" === 2); // false
--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,34 +2,35 @@
    
 const name = "Wasique"
 const count = 35
-// console.log(name + count + " Value");
+//  console.log(name + count + " Value");
 
-console.log(`My name is ${name} and count is ${count}`);
+// console.log(`My name is ${name} and count is ${count}`);
 
 const newName = new String('wasique-ws co com')
 
-// console.log(newName[0]);
-// console.log(newName.__proto__);
+// console.log(newName[0]); // first letter is 'w'
+// console.log(newName.__proto__); // 
 
-console.log(newName.length);
-console.log(newName.toUpperCase());
-console.log(newName.charAt(4));
-console.log(newName.indexOf('s'));
+// console.log(newName.length); // length of String
+// console.log(newName.toUpperCase());
+//  console.log(newName.charAt(4)); // q 
+// console.log(newName.indexOf('s')); // 2
 
-const newString = newName.substring(1,9)
-console.log(newString);
+const newString = newName.substring(1,9) 
+// console.log(newString); // asique-w
 
-const anotherStr = newName.slice(-9,4)
-console.log(anotherStr);
+const anotherStr = newName.slice(-8,-4)
+//  console.log(anotherStr); // s co
 
 const newStr = "    waasique    "
-console.log(newStr);
-console.log(newStr.trim());
+// console.log(newStr);
+// console.log(newStr.trim()); // waasique
 
 const url = "https://wasique.com/wasique%20"
 
-console.log(url.replace('%20','25'));
-
-console.log(url.includes('haziq'));
-
+// console.log(url.replace('%20','25'));
+// replace %20 by 25
+//  console.log(url.includes('haziq')); 
 console.log(newName.split(' '))
+
+// split method seperates the string using some seperators such as space, comma,hyphen,etc etc..
