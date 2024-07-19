f587cecafce5371a70ad7693e46033f0effdcbae
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-07-19 13:37:58 +0000

    Number And Maths in Javascript

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -1,37 +1,37 @@
 //   ++++++++++++++++  NUMBER +++++++++++++++
 
 const score = 100
-console.log(score);
+// console.log(score);
 
  const balance = new Number(400.45687)
-console.log(balance);
-console.log(balance.toString());
-console.log(typeof balance);
+// console.log(balance); // [Number: 400.45687]
+// console.log(balance.toString());
+// console.log(typeof balance); // object
 
-console.log(balance.toFixed(2));
-console.log(balance.toPrecision(4));
+// console.log(balance.toFixed(2)); // 400.46
+// console.log(balance.toPrecision(4)); // 400.5
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
 
 
 // ++++++++++++++++++ MATH IN JS ++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-5));
+// console.log(Math); // Object [Math] {}
+// console.log(Math.abs(-6)); // (6)
 // Note: Only Negative Sign changed.
 
-console.log(Math.round(4.49));
-console.log(Math.ceil(4.1));
-console.log(Math.floor(4.9));
-console.log(Math.min(5,7,2,1,0));
-console.log(Math.max(5,7,2,1,0));
+// console.log(Math.round(4.49)); // 4. / (4.5)=> 5
+// console.log(Math.ceil(4.1));//It gives next value(5) 
+// console.log(Math.floor(4.9));
+// console.log(Math.min(5,7,2,1,0));
+// console.log(Math.max(5,7,2,1,0));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random()); // It gives random values
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
 const min =10
 const max =20
 
-console.log(Math.floor(Math.random()*(max-min+ 1)) +min);
+// console.log(Math.floor(Math.random()*(max-min+ 1))+min);
