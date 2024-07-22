26db0907be72b67ef75f0f99fabb1a29bc4faa5e
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-07-22 16:16:23 +0000

    Control Flow

---
+++ 04_control_flow/01_if_else.js
@@ -0,0 +1,48 @@
+// If else 
+// Comparison operators
+// <,>,<=,>=,==,!=,===,!==
+
+// const isUserLoggedIn = true
+// if (isUserLoggedIn) {
+//     console.log("Welcome");
+// }
+
+// if ( 4 == "4") {
+//     console.log("Equal");
+// }
+
+// const temperature = 37
+// if (temperature !== 37) {
+//     console.log("temperature is 37 degree");
+// }else {
+//     console.log("temperature is not 37 degree");
+// }
+
+// const score = 200
+// if (score>100) {
+//     const power = "fly"
+//     console.log(`User power: ${power} `);
+// }
+// console.log(`User power: ${power} `);
+
+// const balance = 1000
+// if (balance>500)console.log("correct"),console.log("incorrect");
+// It is not write syntax..
+
+// const balance = 5000
+// if (balance > 1000) {
+//     console.log("Greater than 1000");
+// } else if (balance > 2000) {
+//     console.log("Greater than 2000");
+// } else if (balance > 3000) {
+//     console.log("Greater than 3000 ");
+// } else {
+//     console.log("Greater than 5000");
+// }
+
+const userLoggedIn = true
+const cardNumber = true
+
+if (userLoggedIn && cardNumber && 3 === "3") {
+    console.log("Allow to login");
+}
