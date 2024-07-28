14a2161715c9acd2d01b7511daaf273a4442441a
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-07-28 19:56:03 +0000

    while and do_while loop

---
+++ 05_iterations/02_while_doWhile.js
@@ -0,0 +1,20 @@
+
+// let index = 0
+// while (index <= 20) {
+//     console.log(`value of index is: ${index}`);
+//     index = index + 2
+// }
+
+// const myArray = ['Javascript', 'Python','C++']
+// let myArr = 0 
+// while (myArr < myArray.length) {
+//     console.log(`value is ${myArray[myArr]}`);
+//     myArr = myArr + 1
+// }
+
+// let score = 12
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+
+// } while (score <= 10);
