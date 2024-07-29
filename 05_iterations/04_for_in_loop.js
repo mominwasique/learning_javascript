11c25850b7486bd0f985fc5bfba99961220b02fa
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-07-29 16:21:24 +0000

    for in loop

---
+++ 05_iterations/04_for_in_loop.js
@@ -0,0 +1,37 @@
+// FORIN LOOP
+
+// applying on objects
+// const myObject = {
+//     RR : "Rajasthan Royals",
+//     SRH : "Sunrisers Hyderabad",
+//     MI : "Mumbai Indians"
+// }
+
+// for (const key in myObject) {
+//     console.log(`Shortcut of ${key} is ${myObject[key]}`);
+    
+// }
+
+// applying on arrays
+const array = [2,4,6,8,10]
+
+// for (const value in array) {
+//     console.log(value);
+//     // it gives keys : 0 1 2 3 4
+// }
+
+// for (const key in array) {
+//     console.log(`Key ${key} :- ${array[key]}`);
+// }
+
+// const map = new Map()
+
+// map.set('js', "Javascript")
+// map.set('java', "Java")
+// map.set('py', "Python")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+// map is not iterable using forin loop
