6749cf1b54251c25308c995248b372da2de1dd2d
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-07-29 16:45:53 +0000

    foreach loop

---
+++ 05_iterations/05_foreach_loop.js
@@ -0,0 +1,38 @@
+const coding = ["js","c++","java","python"]
+
+// coding.forEach( function(item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (val) => {
+//     console.log(val);
+// } )
+
+// function execute(item){
+//     console.log(item);
+// }
+
+// coding.forEach(execute)
+
+// coding.forEach( (item,index,arr) => {
+//     console.log(item,index,arr);
+// } )
+
+// const programming = [
+//     {
+//         languageName: "Javascript",
+//         languageFileName: ".js"
+//     },
+//     {
+//         languageName: "Java",
+//         languageFileName: ".java"
+//     },
+//     {
+//         languageName: "Python",
+//         languageFileName: ".py"
+//     }
+// ]
+
+// programming.forEach( (item) => {
+//     console.log(`${item.languageName} - ${item.languageFileName}`);
+// } )
