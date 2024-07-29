b3fcbaf7d27f2f38c2bab5ee9ab6378c671ea179
Mohd Wasiqurrahim Mohd Shafiqurrahim <99969469+mominwasique@users.noreply.github.com>
2024-07-29 16:21:04 +0000

    for of loop

---
+++ 05_iterations/03_for_of_loop.js
@@ -0,0 +1,52 @@
+
+// ["","",""]
+// [{},{},{}]
+
+// const numbers = [1, 2, 3, 4, 5]
+
+// for (const num of numbers) {
+//     console.log(num);
+// }
+
+// const greetings_1 = "Hello World!"
+
+// for (const greet of greetings) {
+    
+//     console.log(`Each char is ${greet}`);
+// }
+// const greetings_2 = "Hello World!"
+
+
+// for (const greet of greetings) {
+//     if (greet == " ") {
+//         continue
+//     }
+//     console.log(`Each char is ${greet}`);
+// }
+
+//MAP
+
+// const map = new Map()
+
+// map.set('js', "Javascript")
+// map.set('java', "Java")
+// map.set('py', "Python")
+
+// for (const key of map) {
+//     console.log(key);
+// }
+
+// for (const [key, value] of map) {
+//     console.log(`${key} : ${value}`);
+// }
+
+// const myObj = {
+    // CSK : 'Chennai Super Kings',
+    // MI : 'Mumbai Indians',
+// }
+
+// for (const key of myObj) {
+    // console.log(key);
+// }
+
+// Object is not iterable using forof loop
